f46721e59b44a151ae61bb6e142a7cdc4631051a
liweidong <memet@192.168.66.21>
2021-01-21 15:40:11 +0800

    语料转化

--- dee/utils.py
+++ dee/utils.py
@@ -3,6 +3,9 @@
 # DATE: 19-9-19
 
 import json
+import jieba
+import jieba.posseg as pseg
+jieba.enable_paddle()
 import logging
 import pickle
 from pytorch_pretrained_bert import BertTokenizer
@@ -89,4 +92,87 @@ def strtobool(str_val):
     else:
         raise ValueError("invalid truth value %r" % (str_val,))
 
+import copy
 
+def main():
+    """
+
+    """
+    notice_id = ''
+
+    out_file = open('../Data/train_label_puddle.txt', 'w+')
+    temp_json = default_load_json("../Data/train.json")
+    for notice in temp_json:
+        notice_id = notice[0]
+        notice_sentence = notice[1]['sentences']
+        notice_avm = notice[1]['ann_valid_mspans']
+        notice_avd = notice[1]['ann_valid_dranges']
+        notice_m2d = notice[1]['ann_mspan2dranges']
+        notice_m2g = notice[1]['ann_mspan2guess_field']
+        notice_ree = notice[1]['recguid_eventname_eventdict_list']
+
+        # 知识标注
+        notice_sentence_label = []
+        for i in range(len(notice_sentence)):
+            sentence_label = ['O'] * len(notice_sentence[i])
+            notice_sentence_label.append(sentence_label)
+
+        for k in notice_m2d:
+            v = notice_m2d[k]
+            label = notice_m2g[k]
+            for rg in v:
+                sentence_index = rg[0]
+                begin_pos = rg[1]
+                end_pos = rg[2] - 1
+                notice_sentence_label[sentence_index][begin_pos] = 'b-' + label
+                notice_sentence_label[sentence_index][end_pos] = 'e-' + label
+                notice_sentence_label[sentence_index][begin_pos + 1:end_pos] = ['m-' + label] * (end_pos - begin_pos - 1)
+
+        # 分词词性标注
+        notice_sentence_pos = []
+        for i in range(len(notice_sentence)):
+            sentence_label = ['O'] * len(notice_sentence[i])
+            notice_sentence_pos.append(sentence_label)
+        word_list = []
+        pos_list = []
+        for sentence_index, sentence in enumerate(notice_sentence):
+            words = pseg.cut(sentence, use_paddle=True)
+            word_list = []
+            pos_list = []
+            begin_pos = 0
+            end_pos = 0
+            cur_pos = 0
+            for word, pos in words:
+                word_list.append(word)
+                pos_list.append(pos)
+                begin_pos = cur_pos
+                end_pos = cur_pos + len(word) - 1
+                cur_pos += len(word)
+                if len(word) > 1:
+                    notice_sentence_pos[sentence_index][begin_pos] = 'b-' + pos
+                    notice_sentence_pos[sentence_index][end_pos] = 'e-' + pos
+                    notice_sentence_pos[sentence_index][begin_pos + 1:end_pos] = ['m-' + pos] * (
+                                end_pos - begin_pos - 1)
+                else:
+                    notice_sentence_pos[sentence_index][begin_pos] = 'i-' + pos
+
+
+
+        for event in notice_ree:
+            event_info = event[2]
+            pass
+
+        # 句子实体标注输出
+        for i, sentence in enumerate(zip(notice_sentence_label, notice_sentence_pos)):
+            for j, c in enumerate(zip(notice_sentence_label[i], notice_sentence_pos[i])):
+                unit = notice_sentence[i][j] + ' ' + notice_sentence_pos[i][j] + ' ' + notice_sentence_label[i][j]
+                out_file.write(unit + '\n')
+            out_file.write('#*#' + ' ' + 'sen_split' + ' ' + 'sen_split' + '\n')
+        out_file.write('#****#' + ' ' + 'notice_split' + ' ' + 'notice_split' + '\n')
+
+    out_file.close()
+
+    return notice_id
+
+if __name__ == "__main__":
+    tmp_json = main()
